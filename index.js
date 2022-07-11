417d047c4455b2bf023c9578715a91149fbccc58
= <michaelviloria.g@gmail.com>
2022-07-11 14:23:47 -0500

    Desafio 'API NoRestfull Plantillas' finalizado

--- index.js
+++ index.js
@@ -1,91 +1,36 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
 
-const exphbs = require("express-handlebars");
+const routes = require("./src/routes/routes"); // rutas 
 
-// <------------------------- Contenedor de Productos ------------------------->
-
-const Contenedor = require("./contenedor");
-const contenedor1 = new Contenedor("productos.json");
+app.use(express.static("public"));
+app.use("/api/productos", routes);
 
 // <------------------------- Configuracion de HandleBars ------------------------->
 
-app.set("views", "./views");
-app.engine(
-	".hbs",
-	exphbs({
-		defaultLayout: "main",
-		layoutsDir: "./views/layouts",
-		partialsDir: "./views/partials",
-		extname: ".hbs",
-	})
-);
-app.set("view engine", ".hbs");
+// const exphbs = require("express-handlebars");
 
-// <------------------------- Configuracion Rutas ------------------------->
+// app.set("views", "./views/handlebars");
+// app.engine(
+//   ".hbs",
+//   exphbs({
+//     defaultLayout: "main",
+//     layoutsDir: "./views/handlebars/layouts",
+//     partialsDir: "./views/handlebars/partials",
+//     extname: ".hbs",
+//   })
+// );
+// app.set("view engine", ".hbs");
 
-app.use("/api/productos", router);
-router.use(express.json());
-router.use(express.urlencoded({ extended: true }));
+// <------------------------- Configuracion de Pug ------------------------->
 
-app.use(express.static("public"));
+// app.set("view engine", ".pug");
+// app.set("views", "./views/pug");
 
-// <------------------------- Rutas ------------------------->
+// <------------------------- configuracion de EJS ------------------------->
 
-router.get("/", (req, res) => {
-	const { id } = req.query;
-	if (id != undefined) {
-		const response = contenedor1.get(Number(id));
-		res.render("idProduct", { response });
-	} else {
-		const response = contenedor1.getAll();
-		res.render("index", { response });
-	}
-});
-
-router.get("/:id", (req, res) => {
-	const { id } = req.params;
-	const response = contenedor1.get(Number(id));
-	res.render("idProduct", { response });
-});
-
-router.post("/", (req, res) => {
-	const { nombre, precio, imagen } = req.body;
-	if (nombre === "" || precio === "" || imagen === "") {
-		res.render("addProduct", {response: false});
-	} else {
-    const response = contenedor1.save({
-      nombre: nombre,
-      precio: Number(precio),
-      imagen: imagen,
-    });
-		res.render("addProduct", {response});
-	}
-});
-
-router.put("/:id", (req, res) => {
-  const { nombre, precio, imagen } = req.body;
-  const { id } = req.params;
-	if (nombre === "" || precio === "" || imagen === "") {
-		res.render("updateProduct", {response: false});
-	} else {
-    const response = contenedor1.update(
-      Number(id), 
-      {
-        nombre: nombre,
-        precio: Number(precio),
-        imagen: imagen
-      });
-		res.render("updateProduct", {response});
-	}
-});
-
-router.delete("/:id", (req, res) => {
-	const { id } = req.params;
-  const response = contenedor1.delete(Number(id));
-	res.render("deleteProduct", {response});
-});
+app.set("view engine", ".ejs");
+app.set("views", "./views/ejs");
 
 // <------------------------- Servidor ------------------------->
 
